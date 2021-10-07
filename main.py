4ce83f94cf19ead2874a50da3e86c7a40e00c616
mici <76863251+miciella@users.noreply.github.com>
2021-10-07 17:10:40 +0800

    Add files via upload
    Hirst painting

--- main.py
+++ main.py
@@ -1,52 +1,52 @@
-from turtle import Screen
-from snake import Snake
-from food import Food
-from scoreboard import Scoreboard
-import time
+import random
+import colorgram
+import turtle as t
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("Snake Game")
-screen.tracer(0) # turn off the tracer
+timmy = t.Turtle()
 
-snake = Snake()
-food = Food()
-scoreboard = Scoreboard()
+'''Extract color from image'''''
+# colors = colorgram.extract('hirst.jpg', 25)
+#
+# rgb_colors = []
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     # store in tuple
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
 
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.right, "Right")
-screen.onkey(snake.left, "Left")
+'''RGB colors extracted'''
+list_of_colors = [
+    (199, 175, 117), (124, 36, 24), (168, 106, 57), (186, 158, 53), (6, 57, 83), (109, 67, 85),
+    (113, 161, 175), (22, 122, 174), (64, 153, 138), (39, 36, 36), (76, 40, 48), (9, 67, 47),
+    (90, 141, 53), (181, 96, 79), (132, 40, 42), (210, 200, 151), (141, 171, 155), (179, 201, 186),
+    (172, 153, 159), (212, 183, 177), (176, 198, 203)
+]
+t.colormode(255)
+color = random.choice(list_of_colors)
+timmy.speed("fastest")
+timmy.penup()
+timmy.hideturtle()
+# between 270 and 360 degrees from center
+timmy.setheading(225)
+# to adjust the position
+timmy.forward(300)
+timmy.setheading(0)
 
-game_is_on = True
+number_of_dots = 100
+for dot_count in range(1, number_of_dots+1):
+    timmy.dot(20, random.choice(list_of_colors))
+    timmy.forward(50)
 
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+    if dot_count % 10 == 0:
+        timmy.setheading(90)
+        timmy.forward(50)
+        timmy.setheading(180)
+        timmy.forward(500)
+        timmy.setheading(0)
 
-    # Detect collision with the food
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
-
-    # Detect collision with the wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        scoreboard.reset_score()
-        snake.reset_snake()
-
-    # Detect collision with the tail
-    # for segment in snake.segments[1:]:
-    #     if snake.head.distance(segment) < 10:
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
-            scoreboard.reset_score()
-            snake.reset_snake()
-
-
+screen = t.Screen()
 screen.exitonclick()
